b3a1278c21ded57292b0afeee55f7a0dd287d028
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2018-08-26 04:23:36 +0200

    Edit DE.lua
    Complete DE Translation

--- locales/de.lua
+++ locales/de.lua
@@ -1,49 +1,73 @@
 Locales['de'] = {
   -- Cloakroom
-  ['cloakroom'] = 'Umkleide',
-  ['ems_clothes_civil'] = 'Zivilkleidung',
-  ['ems_clothes_ems'] = 'Arbeitskleidung',
+  ['cloakroom'] 							= 'Umkleide',
+  ['ems_clothes_civil'] 					= 'Zivilkleidung',
+  ['ems_clothes_ems'] 						= 'Arbeitskleidung',
+  
   -- Vehicles
-  ['veh_menu'] = 'Fahrzeuge',
-  ['veh_spawn'] = 'Drücke ~INPUT_CONTEXT~ um ein Fahrzeug zu bekommen',
-  ['store_veh'] = 'Drücke ~INPUT_CONTEXT~ um das Fahrzeug zu parken',
-  ['ambulance'] = 'Krankenwagen',
-  ['helicopter'] = 'Helicopter',
+  ['veh_menu'] 								= 'Fahrzeuge',
+  ['veh_spawn'] 							= 'Benutze ~INPUT_CONTEXT~ um ein Fahrzeug zu bekommen.',
+  ['store_veh'] 							= 'Benutze ~INPUT_CONTEXT~ um das Fahrzeug zu ein zu parken.',
+  ['ambulance'] 							= 'Rettungswagen',
+  ['helicopter'] 							= 'Hubschrauber',
+  
   -- Action Menu
-  ['hospital'] = 'Krankenhaus',
-  ['revive_inprogress'] = 'wiederbeleben im Gange',
-  ['revive_complete'] = 'du wurdest wiederbelebt ~y~%s~s~',
-  ['revive_complete_award'] = 'du wurdest wiederbelebt ~y~%s~s~, ~g~$%s~s~',
-  ['heal_inprogress'] = 'you are healing!',
-  ['heal_complete'] = 'you have healed ~y~%s~s~',
-  ['no_players'] = 'keine Spieler in der Nähe',
-  ['no_vehicles'] = 'keine Fahrzeuge in der Nähe',
-  ['player_not_unconscious'] = 'ist nicht im Koma',
-  ['player_not_conscious'] = 'that player is not conscious!',
+  ['hospital'] 								= 'Krankenhaus',
+  ['revive_inprogress'] 					= 'Wiederbeleben im Gange...',
+  ['revive_complete'] 						= 'Du wurdest wiederbelebt ~y~%s~s~!',
+  ['revive_complete_award'] 				= 'Du wurdest wiederbelebt ~y~%s~s~, ~g~$%s~s~!',
+  ['heal_inprogress'] 						= 'Du verarztest!',
+  ['heal_complete'] 						= 'Du hast verarztest ~y~%s~s~',
+  ['no_players'] 							= 'Keine Anwohner in der Nähe!',
+  ['no_vehicles'] 							= 'Keine Fahrzeuge in der Nähe!',
+  ['player_not_unconscious'] 				= 'Ist nicht im Koma...versuche etwas Anderes!',
+  ['player_not_conscious'] 					= 'Dieser Anwohner ist nicht ohnmächtig!',
+  
   -- Boss Menu
-  ['deposit_society'] = 'Notdienst einzahlen',
-  ['withdraw_society'] = 'Notdienst abheben',
-  ['boss_actions'] = 'boss actions',
+  ['deposit_society'] 						= 'Auf das Rettungsdienstkonto einzahlen',
+  ['withdraw_society'] 						= 'Vom Rettungsdienstkonto abheben',
+  ['boss_actions'] 							= 'Vorgesetzten Aktionen',
+  
   -- Misc
-  ['invalid_amount'] = '~r~ungültiger Betrag',
-  ['open_menu'] = 'Drücke ~INPUT_CONTEXT~ um das Menü zu öffnen',
-  ['deposit_amount'] = 'Betrag zum einzahlen',
-  ['money_withdraw'] = 'Betrag zum abheben',
-  -- F6 Menu
-  ['ems_menu'] = 'Notdienst - Menü',
-  ['ems_menu_title'] = 'Notdienst - Menü',
-  ['ems_menu_revive'] = 'Spieler wiederbeleben',
-  ['ems_menu_putincar'] = 'in Fahrzeug setzen',
+  ['invalid_amount'] 						= '~r~Ungültiger Betrag',
+  ['open_menu'] 							= 'Benutze ~INPUT_CONTEXT~ um das Menü zu öffnen.',
+  ['deposit_amount'] 						= 'Betrag zum einzahlen',
+  ['money_withdraw'] 						= 'Betrag zum abheben',
+  ['fast_travel'] 							= 'Benutze ~INPUT_CONTEXT~ um zu reisen.',
+  ['open_pharmacy'] 						= 'Benutze ~INPUT_CONTEXT~ um auf die Apotheke zuzugreifen.',
+  ['pharmacy_menu_title'] 					= 'Apotheke',
+  ['pharmacy_take'] 						= 'Nehmen',
+  ['medikit'] 								= 'Medikit',
+  ['bandage'] 								= 'Verbände',
+  ['max_item'] 								= 'Du trägst bereits genug Sanitätsmaterial!',
+  
+  -- F9 Menu
+  ['ems_menu'] 								= 'Rettungsdienst - Menü',
+  ['ems_menu_title'] 						= 'Rettungsdienst - Menü',
+  ['ems_menu_revive'] 						= 'Anwohner wiederbeleben',
+  ['ems_menu_putincar'] 					= 'Ins Fahrzeug setzen',
+  ['ems_menu_small'] 						= 'Wundversorgung',
+  ['ems_menu_big'] 							= 'Intensive Behandlung',
+  
   -- Phone
-  ['alert_ambulance'] = 'Notdienst anrufen',
+  ['alert_ambulance'] 						= 'Rettungsdienst anrufen',
+  
   -- Death
-  ['please_wait'] = 'you will bleed out in ~b~%s minutes %s seconds~s~\n',
-  ['press_respawn'] = 'press [~b~E~w~] to respawn.',
-  ['respawn_now_fine'] = 'respawn now for ~g~$%s ~w~[Press ~b~E~w~]',
-  ['respawn_fine'] = 'you paid ~r~$%s~s~ to respawn.',
-  ['distress_send'] = 'press [~b~G~s~] to send distress signal',
-  ['distress_sent'] = 'distress signal has been sent to available units!',
-  ['distress_message'] = 'medical attention required: unconscious citizen!',
+  ['please_wait'] 							= 'Du verblutest in ~b~%s Minuten %s Sekunen~s~\n',
+  ['Benutze_respawn'] 						= 'Benutze [~b~E~w~] zum respawnen [-5.000$].',
+  ['respawn_now_fine'] 						= 'Respawn für ~g~$%s ~w~[Benutze ~b~E~w~]',
+  ['respawn_fine'] 							= 'Für den Respawn hast Du ~r~$%s~s~ bezahlt!',
+  ['distress_send'] 						= 'Benutze [~b~G~s~] um ein Notsignal abzusenden!',
+  ['distress_sent'] 						= 'Notsignal wurde an alle verfügbaren Rettungskräfte gesendet!',
+  ['distress_message'] 						= 'Rettungsdienstliche Hilfe erforderlich! Verletzter Anwohner!',
+  
   -- Revive
-  ['revive_help'] = 'Spieler wiederbeleben',
+  ['revive_help'] 							= 'Anwohner wiederbeleben...',
+  
+  -- Item
+  ['used_medikit']							= 'Du hast ~y~1x~s~ Medikit benutzt!',
+  ['used_bandage']							= 'Du hast ~y~1x~s~ Verband benutzt!',
+  ['not_enough_medikit'] 					= 'Du hast kein ~b~Medikit~w~ dabei!',
+  ['not_enough_bandage'] 					= 'Du hast keinen ~b~Verband~w~ dabei!',
+  ['healed'] 								= 'Du wurdest behandelt!',
 }
